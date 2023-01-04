0087ed164dade183f40ede0f94661b2ca4929a15
Imre Foldes <imre.foldes@ascendaloyalty.com>
2023-01-04 12:11:44 +0100

    Add cache for var attribute lookup

--- lib/core_ext/deep_fetch.rb
+++ lib/core_ext/deep_fetch.rb
@@ -1,8 +1,41 @@
 require 'backport_dig' if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.3')
+require 'singleton'
+
+class VarCache
+
+  include Singleton
+
+  def self.fetch_or_store(key)
+    instance.fetch_or_store(key)
+  end
+
+  def initialize
+    @storage = {}
+  end
+
+  def fetch(key)
+    @storage[key]
+  end
+
+  def store(key)
+    @storage[key] = attributes(key)
+  end
+
+  def fetch_or_store(key)
+    fetch(key) || store(key)
+  end
+
+  private
+
+    def attributes(key)
+      key.to_s.split('.')
+    end
+
+end
 
 class Hash
   def deep_fetch(key, default = nil)
-    keys = key.to_s.split('.')
+    keys = VarCache.fetch_or_store(key)
     value = dig(*keys) rescue default
     value.nil? ? default : value  # value can be false (Boolean)
   end
@@ -10,7 +43,7 @@ end
 
 class Array
   def deep_fetch(index, default = nil)
-    indexes = index.to_s.split('.').map(&:to_i)
+    indexes = VarCache.fetch_or_store(index).map(&:to_i)
     value = dig(*indexes) rescue default
     value.nil? ? default : value  # value can be false (Boolean)
   end
